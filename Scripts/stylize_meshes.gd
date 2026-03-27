faf883fdfa1f885c273071482c85b00fcd2215f1
Gryyre <gryyre@protonmail.com>
2026-03-27 16:15:43 -0400

    Edited kid's room

--- Scripts/stylize_meshes.gd
+++ Scripts/stylize_meshes.gd
@@ -3,11 +3,16 @@ extends Node3D
 @export var shader_material: ShaderMaterial
 @export_color_no_alpha var mesh_color
 
+const BASIC_MATERIAL = preload("res://3D Art/Generic Materials/Basic Material.tres")
+
 func _ready() -> void:
 	for node: Node in get_children():
 		if not (node is MeshInstance3D): continue
 		
 		var mesh: Mesh = node.mesh
+		
+		if not mesh.surface_get_material(0):
+			mesh.surface_set_material(0, BASIC_MATERIAL)
 		var mesh_material: StandardMaterial3D = mesh.surface_get_material(0)
 		
 		mesh_material.albedo_color = mesh_color
