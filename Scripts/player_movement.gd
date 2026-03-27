495f396fd286609e45aca307f9c4e1320c0acf40
Gryyre <gryyre@protonmail.com>
2026-03-27 14:17:18 -0400

    Added keyboard camera control

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -4,18 +4,30 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 var mouse_sensitivity = 0.005
+var keyboard_camera_sensitivity = 10
 @onready var camera: Camera3D = %Camera
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		# Rotate body for horizontal rotation
-		rotate_y(-event.relative.x * mouse_sensitivity)
-		
-		# Rotate camera for vertical rotation
-		camera.rotate_x(-event.relative.y * mouse_sensitivity)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+		rotate_camera(event.relative)
+
+func rotate_camera(input: Vector2) -> void:
+	# Rotate body for horizontal rotation
+	rotate_y(-input.x * mouse_sensitivity)
+	
+	# Rotate camera for vertical rotation
+	camera.rotate_x(-input.y * mouse_sensitivity)
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+
+# This makes it easier to use the camera while walking if playing with a trackpad
+func handle_keyboard_camera_control() -> void:
+	var camera_vector: Vector2 = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
+	camera_vector *= keyboard_camera_sensitivity
+	rotate_camera(camera_vector)
 
 func _physics_process(delta: float) -> void:
+	handle_keyboard_camera_control()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
